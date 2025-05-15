841729bd21f8d00696885bd9e0a6f7d61974a0a4
anton2920 <anton2920@gmail.com>
2025-05-15 19:50:34 +0300

    Added solution to problem #2

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@ ALEF=$HOME/Projects/alef-plan9port
 PATH=$ALEF/bin:$PATH; export PATH
 
 for file in `ls *.l`; do
-	8al -o $file.o -I$ALEF/include/alef $file
+	8al -w -o $file.o -I$ALEF/include/alef $file
 done
 
 8l -o $PROJECT -L $ALEF/lib/alef *.o -lA
